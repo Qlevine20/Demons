7b0c977a505ade3dae6ab7f863233265d5a568e0
Quinn Levine <Qlevine@uci.edu>
2015-07-17 00:22:21 -0700

    Mostly worked on the first enemy.  It know switches gravity after a set time and shoots little fireballs.  It also switches the direction its facing when gravity switches.  Cool looking.

---
+++ Demons/Assets/OurScripts/Hopper.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Hopper : MonoBehaviour {
+
+
+	public Rigidbody2D projectile;
+	public float projectilespeed;
+	public int timetochange;
+	// Use this for initialization
+	private Rigidbody2D rb;
+	private float count;
+	private bool ready;
+	private int currcount;
+
+	void Start(){
+		rb = GetComponent<Rigidbody2D> ();
+		count = 0;
+		rb.gravityScale = 1;
+	}
+	// Update is called once per frame
+	void Update () {
+		count +=Time.deltaTime;
+		if (UnityEngine.Random.Range (1, 100) < 5) {
+			Rigidbody2D clone;
+			clone = Instantiate(projectile,this.transform.position,Quaternion.identity) as Rigidbody2D;
+			if(UnityEngine.Random.Range (0,2)==1){
+				clone.velocity = new Vector2(projectilespeed,0);
+			}
+			else{
+				clone.velocity = new Vector2(-(projectilespeed),0);
+			}
+		}
+		if ((int)(Math.Round (count)) % timetochange == 0 && count>0&&ready) {
+
+			rb.gravityScale=-(rb.gravityScale);
+			transform.localScale= new Vector2(transform.localScale.x,-(transform.localScale.y));
+			ready=false;
+			currcount = (int)Math.Round (count);
+		}
+		if (!ready&& (int)Math.Round (count)== currcount+3) {
+			ready=true;
+		}
+
+	}
+}
---
+++ Demons/Assets/OurScripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public GameObject hopper;
+
+	void Start(){
+		if (this.GetComponent <Collider2D> () != null) {
+			Physics2D.IgnoreCollision (this.GetComponent<Collider2D> (), hopper.GetComponent<Collider2D> ());
+		}
+	}
+
+	// Use this for initialization
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag != "Hopper") {
+			Destroy (this.gameObject);
+		}
+	}
+
+}
