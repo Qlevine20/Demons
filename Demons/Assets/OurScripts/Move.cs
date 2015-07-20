dfead4c76fdd1a7ee069b64cc1d3209ecbdcfd93
Quinn Levine <Qlevine@uci.edu>
2015-07-20 14:21:59 -0700

    Added in jetpack and a fuel level bar so that its easier to get around.

--- Demons/Assets/OurScripts/Move.cs
+++ Demons/Assets/OurScripts/Move.cs
@@ -8,22 +8,40 @@ public class Move : MonoBehaviour {
 	public float movespeed;
 	private Transform tr;
 	private ParticleSystem jetpack;
+	private Rigidbody2D rb;
+	public static float maxfuel;
+	public static float fuelevel;
+	private bool fuelon;
 
 	void Start(){
+		rb = GetComponent<Rigidbody2D> ();
 		tr = GetComponent<Transform> ();
 		jetpack = GetComponent<ParticleSystem> ();
 		jetpack.enableEmission = false;
+		maxfuel = 3;
+		fuelevel = maxfuel;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey (KeyCode.UpArrow)) {
-			tr.Translate((Vector3.up)/7f);
-			jetpack.enableEmission = true;
+			if (fuelevel > 0) {
+				rb.AddForce (new Vector2 (0, movespeed));
+				fuelon = true;
+				fuelevel -= 1 * Time.deltaTime;
+				jetpack.enableEmission = true;
+			} 
+			else {
+				jetpack.enableEmission = false;
+			}
 		}
-
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			tr.Translate ((Vector3.up) / 15f);
+		} 
 		if (Input.GetKeyUp (KeyCode.UpArrow)) {
 			jetpack.enableEmission = false;
+			fuelon = false;
+
 		}
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			tr.Translate((Vector3.left)/20f);
@@ -36,6 +54,15 @@ public class Move : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D coll){
 		if (coll.gameObject.tag == "Exit") {
 			print ("You win");
+		}	
+	}
+
+	void OnCollisionStay2D(Collision2D coll){
+		if (coll.gameObject.tag == "Block") {
+			if (fuelevel < maxfuel) {
+				fuelevel += 1 * Time.deltaTime;
+				print (fuelevel/maxfuel);
+			}
 		}
 	}
 }
